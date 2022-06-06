8d7e522ab07167657a0ea5884b0108db7737603a
mgont <mgontarz20@gmail.com>
2022-06-06 14:51:07 +0200

    training plotter from csv

--- plotter.py
+++ plotter.py
@@ -12,15 +12,15 @@ if __name__ == '__main__':
     # print(df.columns)
 
 
-    fig1, ax = plt.subplots(1,2, figsize = (20,10))
+    fig1, ax = plt.subplots(1,2, figsize = (20,8))
 
-    p1a = ax[0].plot(df['epoch'],df['loss'], 'g--', linewidth = 1)
-    p1b = ax[0].plot(df['epoch'],df['val_loss'], 'r-.', linewidth = 1.2)
+    p1a, = ax[0].plot(df['epoch'],df['loss'], 'g--', linewidth = 1)
+    p1b, = ax[0].plot(df['epoch'],df['val_loss'], 'r-.', linewidth = 1.2)
 
-    p2a = ax[1].plot(df['epoch'], df['SSIMMetric'], 'g--', linewidth = 1.1)
-    p2b = ax[1].plot(df['epoch'], df['val_SSIMMetric'], 'r--', linewidth = 1.2)
+    p2a, = ax[1].plot(df['epoch'], df['SSIMMetric'], 'g--', linewidth = 1.1)
+    p2b, = ax[1].plot(df['epoch'], df['val_SSIMMetric'], 'r--', linewidth = 1.2)
 
-    pb1 = ax[0].plot(np.argmin(df['val_loss']), np.min(df['val_loss']), marker = 'x', markersize = 10, color= 'r')
+    pb1, = ax[0].plot(np.argmin(df['val_loss']), np.min(df['val_loss']), marker = 'x', markersize = 10, color= 'r')
     ax[0].annotate(f'Best Validation Loss: {np.min(df["val_loss"])}\nFor Epoch: {np.argmin(df["val_loss"])} | Saved Model', (np.argmin(df['val_loss']), np.min(df['val_loss'])),
 
             xytext=(np.argmin(df['val_loss']),np.min(df["val_loss"]) + 0.002),
@@ -35,12 +35,12 @@ if __name__ == '__main__':
                    color = 'r')
 
                    )
-    pb2 = ax[1].plot(df["epoch"][162], df["val_SSIMMetric"][162], marker = 'x', markersize = 10, color = 'r')
+    pb2, = ax[1].plot(df["epoch"][162], df["val_SSIMMetric"][162], marker = 'x', markersize = 10, color = 'r')
     ax[1].annotate(
         f'Validation SSIM Metric Value: {np.round(df["val_SSIMMetric"][162], 5)}\nFor Epoch: {df["epoch"][162]} | Saved Model',
         (df["epoch"][162], df["val_SSIMMetric"][162]),
 
-        xytext=(df["epoch"][161], df["val_SSIMMetric"][161] - 0.04),
+        xytext=(df["epoch"][162], df["val_SSIMMetric"][162] - 0.04),
         ha='center',
         bbox=dict(
             boxstyle="round",
@@ -53,6 +53,21 @@ if __name__ == '__main__':
 
         )
 
-    ax[0].legend((p1a, p1b, pb1), ("Training Loss", "Validation Loss", "Best (Saved) Model"), loc = 'best')
-    ax[1].legend((p2a, p2b, pb2), ("Training SSIM Metric", "Validation SSIM Metric", "Best (Saved) Model"), loc = 'best')
+
+    leg1 = ax[0].legend([p1a, p1b, pb1], ["Training Loss", "Validation Loss", "Best (Saved) Model"], loc = 'upper right')
+    leg2 = ax[1].legend([p2a, p2b, pb2], ["Training SSIM Metric", "Validation SSIM Metric", "Best (Saved) Model"], loc = 'lower right')
+
+    ax[0].set_title('Loss Graph (Mean Squared Error)')
+    ax[1].set_title('Metric Graph (SSIM)')
+
+    ax[0].set_xlabel('Epoch No.')
+    ax[0].set_ylabel('Loss Value')
+    ax[1].set_xlabel('Epoch No.')
+    ax[1].set_ylabel('Metric Value')
+
+    fig1.suptitle('Training Graphs', fontsize = 20)
+
+    plt.savefig(os.path.join(os.getcwd(), 'plot_uczenia.png'))
+
+
     plt.show()
